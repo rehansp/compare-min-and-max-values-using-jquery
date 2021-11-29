9cb75263f9b2ebefb43a7cc3e42ae95596728805
Rehan Patel <34427014+rehansp@users.noreply.github.com>
2021-11-29 10:10:27 +0000

    index.html script.js

--- script.js
+++ script.js
@@ -1,57 +1,55 @@
-$(document).ready(function () {
-    $("#minval").keyup(function () {
-      alert("hs");
-		var txtbalance = $('#maxval').val();
-		var txtdays = $('#minval').val();
-		
-		if (txtbalance === "" || txtdays === "") return false;
-		
-        if ( parseInt(txtbalance) < parseInt(txtdays) ) {
-            alert("MaxVal should be always greater");
-        }
-    });
-
-    $("#submit_btn").click(function(){
-      
-      $("#myform").validate({
-        rules:{
-          PurchaseRate : {
-                    required: true
-                },
-          ProcTimeMin : {
-              required: true
-          },
-          ExternalCode : {
-                  required: true
-              },
-          ItemDetais : {
-                    required: true
-                },
-          minval : {
+$(document).ready(function() {
+    
+    $("#valid").click(function () {
+        $("#maxandmin").validate({
+            rules : {
+                PurchaseRate : {
                     required: true
                 },
         
-          maxval : {
-              required: true
-          },
+                ProcTimeMin : {
+                    required: true
+                },
         
-          AreaCode : {
-              required: true
-          }
-        },
-
-        messages : {
+                ExternalCode : {
+                    required: true
+                },
+        
+                ItemDetais : {
+                    required: true
+                },
+        
+                MinLevel : {
+                    required: true,
+                    number : true
+                },
+        
+                MaxLevel : {
+                    required: true,
+                    number : true,
+                },
+        
+                AreaCode : {
+                    required: true
+                },
+            },
+            messages : {
                 PurchaseRate : "This Field is required",
                 ProcTimeMin : "This Field is required",
                 ExternalCode : "This Field is required",
                 ItemDetais : "This Field is required",
-                minval : "This Field is required",
-                maxval : "This Field is required",
-                AreaCode  : "Please select an option from the list"
+                MinLevel : "This Field is required",
+                MaxLevel : "This Field is required",
+                AreaCode  : "Please select an option from the list",
             },
-        submitHandler: function(form) {
-                form.submit(alert("Thanks!"))
+            submitHandler: function(form) {
+                if(parseInt($("#MaxLevel").val()) < parseInt($("#MinLevel").val())){
+                    alert("Max value should be always greater")
+                }else{
+                    form.submit(alert("Thanks!"))
+                }
+                
             }
-      })
+        })
     })
-});
+})
